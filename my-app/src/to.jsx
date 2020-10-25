2de0319d5652a9848947a11c75cae5c79fae2477
shivamrai83 <shivamrai83@gmail.com>
2020-10-25 23:31:44 +0530

    To-Do Added

---
+++ my-app/src/to.jsx
@@ -0,0 +1,86 @@
+import React from 'react';
+
+class To extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            task: "",
+            list: []
+        }
+    }
+
+    updateInput(value) {
+        this.setState({
+            task: value,
+        });
+    }
+
+
+    addItem() {
+        if (this.state.task !== '') {
+            const task = {
+                id: Math.random(),
+                value: this.state.task
+            };
+
+            const list = [...this.state.list];
+            list.push(task);
+
+            this.setState({
+                list,
+                task: ""
+            });
+        }
+    }
+
+
+    deleteItem(key) {
+
+        const list = [...this.state.list];
+
+        const updateList = list.filter(item => item.id !== key);
+        this.setState({
+            list: updateList,
+        });
+    }
+
+    render() {
+        return (
+            <>
+                <div>
+                    <div>
+                        <h1>To-Do List</h1>
+                        <input
+                            type="text"
+                            name="task"
+                            value={this.state.task}
+                            onChange={item => this.updateInput(item.target.value)}
+                        />
+
+                        <button onClick={() => this.addItem()}>Add Task</button>
+                    </div>
+                    <div>
+                        <div>
+                            {/* map over and print items */}
+                            {this.state.list.map(item => {
+                                return (
+                                    <div>
+                                        {item.value}<button onClick={() => this.deleteItem(item.id)}>done</button>
+                                    </div>
+                                )
+                            })}
+                        </div>
+                    </div>
+                </div>
+            </>
+        )
+    }
+
+
+
+
+
+}
+export default To;
